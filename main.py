2bab66aae8eb46ad9202725dd1099484122b040c
Mohammad S.NIAEI <m.shemuni@gmail.com>
2024-08-14 13:15:40 +0300

    requirment fixed

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ tess_xlcs = p.tess()
 fig, ax = plt.subplots()
 
 for i, tess_xlc in enumerate(tess_xlcs):
-    tess_xlc.plot(ax=ax)
+    tess_xlc.normalize().plot(ax=ax)
+    print(type(tess_xlc.normalize()))
+    # break
 
 ax.get_legend().remove()
 plt.show()
