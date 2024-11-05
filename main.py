32773626cbd9ab09b9af6152ce7ef708eb8a13fb
Mohammad NIAEI <m.shemuni@gmail.com>
2024-11-05 13:00:23 +0300

    Update to add to pypi

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from octans import Minima
-from astropy.time import Time, TimeDelta
+from octans import Portal
 
-mnm = Minima(Time.now(), TimeDelta(0.02, format="jd"))
-print(mnm)
-for i in mnm:
-    print(i)
+# Create a portal for "kepler-8"
+portal = Portal.from_name("kepler-8")
+
+# Retrieve light curves
+light_curves = portal.kepler()
+
+# Get the first one
+light_curve = next(light_curves)
+
+# smooth and flatten
+xlc = light_curve.smooth_butterworth_filter().flatten()
+
+# Calculate minima times with different methods
+minimas = {
+    "Mean": xlc.minima_mean(),
+    "Fit": xlc.minima_fit(),
+    "KVW": xlc.minima_kwee_van_woerden(),
+    "Bisector": xlc.minima_bisector(),
+    "fielder": xlc.minima_fielder(),
+}
--- src/octans/__init__.py
+++ src/octans/__init__.py
@@ -7,4 +7,11 @@ from .catalgues import ExoplanetEU
 from .catalgues import NASAExoplanetArchive
 from .catalgues import Period
 
-__all__ = ["XLightCurve", "Sky", "Portal", "Boundaries", "Minima", "ExoplanetEU", "NASAExoplanetArchive", "Period"]
+__all__ = [
+    "XLightCurve", "Sky", "Portal", "Boundaries",
+    "Minima", "ExoplanetEU", "NASAExoplanetArchive", "Period"
+]
+
+__version__ = "0.0.1 Beta"
+__author__ = "Mohammad Niaei"
+__license__ = "GNU/GPL V3"
